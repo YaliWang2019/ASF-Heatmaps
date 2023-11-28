be74b8fa162dfaa20772cd6058f0c1dfa138ab77
d-maltos <dmmaltos@alaska.edu>
2023-11-28 13:03:17 -0900

    Updating heatmap.py code

--- heatmaps.py
+++ heatmaps.py
@@ -1,38 +1,44 @@
 # Import libraries
 import pandas as pd
-from PIL import Image
 import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
 
-# Heatmap function for generating heatmaps
+# Heatmap() function for generating heatmaps
 def Heatmaps():
-    # Read CSV file
+    # Read the CSV file
     df = pd.read_csv('coordinates_dates_100000.csv')
 
-    # Process data to get a frequency map of coordinates
+    # Process the data to get a frequency map of coordinates
     coordinate_counts = df.groupby(['coordinate1_lat', 'coordinate1_lon']).size()
 
-    # 2D grid for the heatmap
-    grid_size = 100
+    # Create a higher resolution 2D grid for the heatmap
+    grid_size = 100  
     heatmap_grid = np.zeros((grid_size, grid_size))
 
-    # Fill grid with frequency data
+    # Fill the grid with frequency data
     for (lat, lon), count in coordinate_counts.items():
-        x = int((lat + 90) / 180 * (grid_size - 1))  # Convert latitude to a grid coordinate
-        y = int((lon + 180) / 360 * (grid_size - 1))  # Convert longitude to a grid coordinate
-
-        # Ensure x and y are within the bounds of the grid
+        x = int((lat + 90) / 180 * (grid_size - 1))
+        y = int((lon + 180) / 360 * (grid_size - 1))
         x = max(0, min(x, grid_size - 1))
         y = max(0, min(y, grid_size - 1))
-
         heatmap_grid[x, y] = count
 
     # Normalize the grid for image intensity
-    heatmap_grid = (255 * heatmap_grid / heatmap_grid.max()).astype(np.uint8)
+    heatmap_grid_normalized = heatmap_grid / heatmap_grid.max()
 
-    # Create and save the heatmap image
-    img = Image.fromarray(heatmap_grid, 'L')
-    img = img.resize((1000, 500))  # Resize 
+    # Apply a colormap (using Matplotlib colormaps)
+    cmap = plt.cm.viridis 
+    colored_heatmap = cmap(heatmap_grid_normalized)
+
+    # Convert to image, rotate, and resize
+    img = Image.fromarray((colored_heatmap[:, :, :3] * 255).astype(np.uint8))
+    img = img.rotate(90) # Rotate
+    img = img.resize((1000, 1000))  # Resize 
+
+    # Save and show the image
+    img.save('heatmap.png')
     img.show()
 
-# Function call(s)
+# Function call
 Heatmaps()
