99c6b8d0ff91fac2af678d3c14b03c3e6c8fd8cf
Lisa Jacklin <lmjacklin@alaska.edu>
2023-11-30 15:07:38 -0900

    More Heatmaps

--- HWColors.py
+++ HWColors.py
@@ -2,29 +2,30 @@
 import pandas as pd
 import numpy as np
 from PIL import Image
+
 import matplotlib.pyplot as plt
-import geopandas as gpd
+from matplotlib.colors import ListedColormap
+import geopandas as gpd #geopandas for the world overlay
 
 # Function to plot world map outline
 def plot_world_map(ax):
     world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
     world.plot(ax=ax, color='none', edgecolor='black', linewidth=1)
 
-# Heatmap() function for generating heatmaps
+# Heatmap() function for generating custom-colored heatmaps
 def Heatmaps():
     # Read the larger CSV file with 800,000 data points
-    df = pd.read_csv('coordinates_dates_2022.csv')  # Make sure to replace with your actual file name
+    df = pd.read_csv('coordinates_dates_2022.csv')  
 
     # Process the data to get a frequency map of coordinates
     coordinate_counts = df.groupby(['coordinate1_lat', 'coordinate1_lon']).size()
 
     # Create a higher resolution 2D grid for the heatmap
-    grid_size = 250  # Increase grid size for more data points
+    grid_size = 150  # Increase grid size for more data points
     heatmap_grid = np.zeros((grid_size, grid_size))
 
     # Fill the grid with frequency data
     for (lat, lon), count in coordinate_counts.items():
-        # Adjust rounding logic for coordinates along the border
         x = int((lat + 90) / 180 * (grid_size - 1))
         y = int((lon + 180) / 360 * (grid_size - 1))
         x = max(0, min(x, grid_size - 1))
@@ -34,28 +35,31 @@ def Heatmaps():
     # Normalize the grid for image intensity
     heatmap_grid_normalized = heatmap_grid / heatmap_grid.max()
 
+    # Rotate the data using NumPy
+    rotated_heatmap = np.rot90(heatmap_grid_normalized)
+
+    # Define custom colors based on the number of data points
+    colors = ['#ffffcc', '#ffeda0', '#fed976', '#feb24c', '#fd8d3c', '#fc4e2a', '#e31a1c', '#bd0026', '#800026']
+
+    # Create a custom colormap
+    custom_cmap = ListedColormap(colors)
+
+    # Determine the extent based on the data range
+    min_lat, max_lat = df['coordinate1_lat'].min(), df['coordinate1_lat'].max()
+    min_lon, max_lon = 1.8*df['coordinate1_lon'].min(), 1.8*df['coordinate1_lon'].max()
+
     # Create a figure and axis
     fig, ax = plt.subplots(figsize=(10, 10))
-
-    # Plot the heatmap
-    cmap = plt.cm.viridis 
-    colored_heatmap = cmap(heatmap_grid_normalized)
-
-    # Convert to image, rotate, and resize
-    img = Image.fromarray((colored_heatmap[:, :, :3] * 255).astype(np.uint8))
-    img = img.rotate(90) # Rotate
-    img = img.resize((1000, 1000))  # Resize 
-
-    img = ax.imshow(colored_heatmap, extent=[-180, 180, -90, 90])
-
-   
 
     # Overlay world map outline
     plot_world_map(ax)
 
+    # Plot the heatmap with custom colors
+    img = ax.imshow(rotated_heatmap, cmap=custom_cmap, extent=[min_lat, max_lat, min_lon, max_lon])
+
     # Save and show the image
-    plt.savefig('heatmap_with_world_map.png')
-    plt.show()
+    plt.savefig('heatmap_OverMap.png')
+    #plt.show()
 
 # Function call
 Heatmaps()
